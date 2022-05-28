bd1bf36e746c41b3075f18a5a19c84a063756b81
Omar Alaaeldein <81417777+OmarAlaaeldein@users.noreply.github.com>
2022-05-28 20:00:52 +0200

    Time: 867 ms (6.48%), Space: 26.1 MB (30.13%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -0,0 +1,9 @@
+class Solution:
+    def containsDuplicate(self, nums) -> bool:
+        dic = dict()
+        for i in range(len(nums)):
+            if dic.get(nums[i], None) == None:
+                dic[nums[i]] = True
+            elif dic.get(nums[i], None) == True:
+                return 'true'
+        print(dic)
