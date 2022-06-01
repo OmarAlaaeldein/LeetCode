51d228695dacfe1b457e9efeb8159d40ec110b95
Omar Alaaeldein <81417777+OmarAlaaeldein@users.noreply.github.com>
2022-06-01 18:59:54 +0200

    Time: 45 ms (51.81%), Space: 13.9 MB (15.72%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.py
@@ -0,0 +1,14 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        x=list(str(x))[::-1]
+        if x[-1]=='-':
+            print(x)
+            x.pop()
+            print(x)
+            s=-1*int(''.join(x))
+        else:
+            s=int(''.join(x))
+        if (s<-2**31) or (s>(2**31)-1):
+            return 0
+        else:
+            return s
