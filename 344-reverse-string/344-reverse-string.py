0f9a16699bc34fc0d06997f6559e497be802ebc2
Omar Alaaeldein <81417777+OmarAlaaeldein@users.noreply.github.com>
2022-07-14 15:53:03 +0200

    Time: 208 ms (91.75%), Space: 18.4 MB (87.48%) - LeetHub

--- 344-reverse-string/344-reverse-string.py
+++ 344-reverse-string/344-reverse-string.py
@@ -5,12 +5,8 @@ class Solution:
         Do not return anything, modify s in-place instead.
         """
         low,high=0,len(s)-1
-        def help(s,low,high):
-            if low<high:
-                s[low],s[high]=s[high],s[low]
-                help(s,low+1,high-1)
-            else:
-                return
-        help(s,low,high)
-        
+        while low<high:
+            s[low],s[high]=s[high],s[low]
+            low+=1
+            high-=1
         
