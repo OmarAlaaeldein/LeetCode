9b023778a68e61b3310b9e56a2449727c17b30da
Omar Alaaeldein <81417777+OmarAlaaeldein@users.noreply.github.com>
2022-07-14 02:48:22 +0200

    Time: 40 ms (79.46%), Space: 13.9 MB (62.97%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.py
+++ 7-reverse-integer/7-reverse-integer.py
@@ -2,9 +2,7 @@ class Solution:
     def reverse(self, x: int) -> int:
         x=list(str(x))[::-1]
         if x[-1]=='-':
-            print(x)
             x.pop()
-            print(x)
             s=-1*int(''.join(x))
         else:
             s=int(''.join(x))
