cae3f09e74040f33b2edc164b072976c0b33a619
Omar Alaaeldein <81417777+OmarAlaaeldein@users.noreply.github.com>
2022-07-14 02:44:26 +0200

    Time: 32 ms (17.49%), Space: 13 MB (95.83%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -2,22 +2,24 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         // O(1) space solution
-        int rows=matrix.size();
-        int cols=matrix[0].size();
+        // O(m)+O(n)+O(mn)+O(mn)+O(m)+O(n)=O(mn) time solution
+        int rows=matrix.size(); // n
+        int cols=matrix[0].size(); // m
         int row0=0; // indicator to save the status of row0
         int col0=0; // indicator to save the status of col0
         for (int i=0;i<cols;i++){
             if (matrix[0][i]==0){
-                row0=1;
+                row0=1; // row will be zeroed after completing inner part
                 break;
             }
         }
         for (int i=0;i<rows;i++){
             if (matrix[i][0]==0){
-                col0=1;
+                col0=1; // col will be zeroed after completing inner part
                 break;
             }
         }
+        
         for (int i=1;i<rows;i++){ // set a dummy index array in 0th row, column
             for (int j=1;j<cols;j++){
                 if (matrix[i][j]==0){
@@ -26,6 +28,7 @@ public:
                 }
             }
         }
+        // inner part
         for (int i=1;i<rows;i++){ // zero stuff
             for (int j=1;j<cols;j++){
                 if (matrix[i][0]==0 or matrix[0][j]==0){
